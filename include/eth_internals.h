9353a2ecac7a3c418d55026a9b71d1e30c492934
pscott <scott.piriou@ledger.fr>
2021-07-05 12:52:08 +0200

    Remove pathLength and bip32Path from sharedRO

--- include/eth_internals.h
+++ include/eth_internals.h
@@ -47,8 +47,7 @@ typedef enum chain_kind_e {
     CHAIN_KIND_WEBCHAIN,
     CHAIN_KIND_THUNDERCORE,
     CHAIN_KIND_FLARE,
-    CHAIN_KIND_THETA,
-    CHAIN_KIND_BSC
+    CHAIN_KIND_THETA
 } chain_kind_t;
 
 typedef struct tokenDefinition_t {
