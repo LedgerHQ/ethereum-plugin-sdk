51c4cad6297a99a220eb09e6d30d229ac02d469c
pscott <scott.piriou@ledger.fr>
2021-08-03 12:13:42 +0200

    Update with EIP-1559 PR

--- include/eth_internals.h
+++ include/eth_internals.h
@@ -66,8 +66,8 @@ typedef struct txInt256_t {
 } txInt256_t;
 
 typedef struct txContent_t {
-    txInt256_t gasprice;
-    txInt256_t startgas;
+    txInt256_t gasprice;  // Used as MaxFeePerGas when dealing with EIP1559 transactions.
+    txInt256_t startgas;  // Also known as `gasLimit`.
     txInt256_t value;
     txInt256_t nonce;
     txInt256_t chainID;
@@ -75,6 +75,7 @@ typedef struct txContent_t {
     uint8_t destinationLength;
     uint8_t v[4];
     uint8_t vLength;
+    bool dataPresent;
 } txContent_t;
 
 typedef struct chain_config_s {
