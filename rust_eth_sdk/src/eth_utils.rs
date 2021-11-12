ebf742a5b8983c7111dd666a469a6906e406fd71
pscott <scott.piriou@ledger.fr>
2021-11-12 15:19:37 +0100

    Add prototypes for Number

--- rust_eth_sdk/src/eth_utils.rs
+++ rust_eth_sdk/src/eth_utils.rs
@@ -1,12 +1,15 @@
 use crate::eth_plugin_interface::{ADDRESS_LENGTH, INT256_LENGTH};
 
+#[derive(Default, PartialEq, Eq)]
 pub struct Address([u8; ADDRESS_LENGTH]);
 
+#[derive(Default)]
 pub struct Number([u8; INT256_LENGTH]);
 
-#[derive(Default)]
-pub struct Amount([u8; INT256_LENGTH]);
+impl Number {
+    fn to_amount(&self, dst: &mut str, ticker: &str, decimals: u8) {}
 
-impl Amount {
-    fn displayable(&self, dst: &str, ticker: &str, decimals: u8) {}
+    fn to_str(&self, dst: &str) {}
+
+    // We should implem add, sub, multiply
 }
