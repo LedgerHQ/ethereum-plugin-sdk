913a3062b3f337c0483f959b24bdd958acd95618
pscott <scott.piriou@ledger.fr>
2021-11-12 17:11:25 +0100

    Add unimplemented

--- rust_eth_sdk/src/eth_utils.rs
+++ rust_eth_sdk/src/eth_utils.rs
@@ -7,9 +7,13 @@ pub struct Address([u8; ADDRESS_LENGTH]);
 pub struct Number([u8; INT256_LENGTH]);
 
 impl Number {
-    fn to_amount(&self, dst: &mut str, ticker: &str, decimals: u8) {}
+    fn to_amount(&self, dst: &mut str, ticker: &str, decimals: u8) {
+        unimplemented!()
+    }
 
-    fn to_str(&self, dst: &str) {}
+    fn to_str(&self, dst: &str) {
+        unimplemented!()
+    }
 
     // We should implem add, sub, multiply
 }
